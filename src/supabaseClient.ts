e02a216d3ca3ce7ca171c7e8e4e40f35b2b72785
PabloIxcamparij <j.pablo.sorto@gmail.com>
2025-02-25 11:51:13 -0600

    fix:
    -Change the access to the database connection.

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-// ⚠️ Reemplaza con tus credenciales de Supabase
-const SUPABASE_URL = "https://tclchhosbcayyvfdbsvh.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRjbGNoaG9zYmNheXl2ZmRic3ZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA0NjQ5OTIsImV4cCI6MjA1NjA0MDk5Mn0.VCKa1bYjrqW7u9iakGScXCL5BAZGnuh28dXTVjqdmKw";
+const SUPABASE_URL : any = process.env.REACT_APP_SUPABASE_URL
+const SUPABASE_ANON_KEY : any = process.env.REACT_APP_SUPABASE_ANON_KEY
 
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
