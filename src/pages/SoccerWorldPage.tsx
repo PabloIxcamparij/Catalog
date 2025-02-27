312ea7b557487a71e7399ffd61ecdd39b00a5bc1
PabloIxcamparij <j.pablo.sorto@gmail.com>
2025-02-26 19:20:20 -0600

    fix: rename the subtitle in SoccerWorldPage

--- src/pages/SoccerWorldPage.tsx
+++ src/pages/SoccerWorldPage.tsx
@@ -24,7 +24,7 @@ export default function SoccerWorldPage() {
       <section className="flex flex-col items-center justify-center gap-4 mt-10">
         <div className="inline-block max-w-lg text-center justify-center">
           <span className={title({ color: "yellow" })}>Fantásticas </span>
-          <span className={title()}>camisas de mundiales</span>
+          <span className={title()}>camisas de selecciones</span>
           <div className={subtitle({ class: "mt-4" })}>
             Con variedad de naciones y tallas
           </div>
